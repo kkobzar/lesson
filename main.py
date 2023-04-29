30dde5ad2ee3744cdc4d735d4db1ec41d271f90b
Kyryl Kobzar <kirilkobzar@gmail.com>
2023-04-29 10:01:17 +0300

    find eyes

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import cv2
 
 
-image = cv2.imread('hooman.jpg')
-print(image)
+image = cv2.imread('monke-hooman.jpeg')
+
+casc = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+eyes = casc.detectMultiScale(image)
+
+
+for (x, y, w, h) in eyes:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 255), 2)
+
 
 cv2.imshow('Hooman', image)
 
